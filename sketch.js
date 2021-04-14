3ce5c8cff1fab976c2c7c4b8ead78014d38900fa
eb391916 <eb391916@ohio.edu>
2021-04-14 12:05:28 -0400

    initial-commit
    initial file upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var song;
+var amp;
+var button;
+
+var volhistory = [];
+
+function toggleSong() {
+    if (song.isPlaying()) {
+        song.pause();
+    } else {
+        song.play();
+    }
+}
+
+function preload() {
+    song = loadSound('19thFloor-BobbyRichards.mp3');
+}
+
+function setup() {
+  createCanvas(200,200);
+
+  button = createButton('toggle')
+  button.mousePressed(toggleSong);
+  
+  amp = new p5.Amplitude();
+
+  song.play();
+}
+
+function draw() {
+  background(0);
+
+  var vol = amp.getLevel();
+  volhistory.pushState(vol);
+
+  ellipse(100,100,200,vol*200);
+  console.log(vol);
+}
